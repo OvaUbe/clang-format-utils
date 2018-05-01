5167e4ce0854d7f6e249d03c2547ac46dba6d1d8
ovaube <trevorismay@gmail.com>
2018-05-01 12:56:14 +0300

    Initial commit.

---
+++ run-clang-format.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+source_dirs=${1}
+source_extensions=${2}
+
+current_dir=`dirname "${BASH_SOURCE[0]}"`
+
+clang_format_version=6.0
+config_file=${current_dir}/.clang-format
+
+do_find_clang_format() {
+    result=`which clang-format-${clang_format_version}` && echo ${result} && exit 0
+    result=`which clang-format` && echo ${result} && exit 0
+    exit 1
+}
+
+find_clang_format() {
+    clang_format=`do_find_clang_format` || exit 1
+
+    actual_version=`${clang_format} --version | grep -oP "version \K(\d\.\d)"`
+    if [[ ${actual_version} != ${clang_format_version} ]]; then exit 1; fi
+
+    echo ${clang_format}
+    exit 0
+}
+
+clang_format=`find_clang_format`
+if [[ $? -ne 0 ]]; then echo "clang-format ${clang_format_version} not found"; exit 0; fi
+echo "Found ${clang_format}"
+
+clang_format_opts="-style=file -assume-filename=${config_file} -i"
+
+src_dirs=(${source_dirs})
+
+file_list=`find "${src_dirs[@]}" -type f | grep -E "^.*\.(${source_extensions})$"`
+for file in $file_list; do
+    ${clang_format} ${clang_format_opts} ${file}
+done
+
+exit 1
